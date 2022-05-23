94a3dd1944fce48b60d589037c807dd9b64b796f
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-05-23 16:47:46 +0900

    add basic structure to popcorn

--- final-pjt-front/finalpjt/src/views/PopcornView.vue
+++ final-pjt-front/finalpjt/src/views/PopcornView.vue
@@ -8,7 +8,14 @@
       <img src="@/assets/gift.jpg" width="200">
     </a>
     <br>
+    <button>응모하기</button>
+    <br>
+    <br>
+    <p>현재 응모자 수: </p>
+    <br><br><br>
     <h3>당첨을 축하드립니다</h3>
+    <p>당첨자:</p>
+
   </div>
 </template>
 
