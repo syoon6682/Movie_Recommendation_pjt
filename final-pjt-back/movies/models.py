69684881d0f860f26560074b695e5742f05b9789
syoon6682 <dev.syoon6682@gmail.com>
2022-05-23 14:32:58 +0900

    data remodeling

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -5,16 +5,25 @@ from accounts.models import User
 
 # Create your models here.
 
-# 아마 잘 안쓸 거 같아서 skeleton 수준으로만 작성
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     overview = models.TextField()
-    popularity = models.FloatField()
     release_date = models.DateField()
+    popularity = models.FloatField()
     vote_count = models.IntegerField()
     vote_average = models.FloatField()
     poster_path = models.CharField(max_length=500)
+    genre_ids = models.ManyToManyField(Genre, related_name="movies")
+
+
 
 
 # 완성도 높인 모델
@@ -22,10 +31,10 @@ class Movie(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length=50)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    rank = models.IntegerField()
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    rank = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
