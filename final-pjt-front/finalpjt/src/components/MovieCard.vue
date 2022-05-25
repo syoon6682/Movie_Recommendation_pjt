e422d5bec05c67ed3a88b3972c9476176fbb99d4
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-05-25 13:07:42 +0900

    update moviecard

--- final-pjt-front/finalpjt/src/components/MovieCard.vue
+++ final-pjt-front/finalpjt/src/components/MovieCard.vue
@@ -4,7 +4,6 @@
     <img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="poster" class="card-img-top">
     <div class="card-body">
       <h3> {{movie.title}}</h3>
-      <p class="card-text">{{movie.overview}}</p>
       <router-link  :to="{ name: 'moviedetail', params: { movieId: movie.id }}">자세히 보기</router-link>
     </div>
   </div>
